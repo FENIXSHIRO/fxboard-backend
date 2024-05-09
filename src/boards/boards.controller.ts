9d8faa3714706f6334ef20eef75201149b40610b
Иван Тетеев <fenixshiro@gmail.com>
2024-05-09 18:39:00 +0300

    refactor: minor changes

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -14,22 +14,14 @@ import { BoardsService } from './boards.service';
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
-  @Get()
+  @Get('all')
   getAllBoards() {
     return this.boardsService.getAllBoards();
   }
 
-  @Get(':userId')
+  @Get('byUser:userId')
   async getBoardsForUser(@Param('userId') userId: string): Promise<Board[]> {
     return this.boardsService.getBoardsForUser(userId);
-  }
-
-  @Post(':userId')
-  async createBoard(
-    @Param('userId') userId: string,
-    @Body('name') name: string,
-  ): Promise<Board> {
-    return this.boardsService.createBoard(name, userId);
   }
 
   @Get(':boardId/items')
@@ -39,6 +31,14 @@ export class BoardsController {
       throw new NotFoundException('Board not found');
     }
     return items;
+  }
+
+  @Post(':userId')
+  async createBoard(
+    @Param('userId') userId: string,
+    @Body('name') name: string,
+  ): Promise<Board> {
+    return this.boardsService.createBoard(name, userId);
   }
 
   @Post(':boardId/items')
