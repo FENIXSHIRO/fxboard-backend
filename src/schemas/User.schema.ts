c747d9137be09dc43b858e0af398c0e0cb68675e
Иван Тетеев <fenixshiro@gmail.com>
2024-05-09 14:05:48 +0300

    feat: добавил user

---
+++ src/schemas/User.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop({ unique: true, required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: false })
+  registerDate?: Date;
+
+  @Prop({ required: false })
+  avatarUrl?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import mongoose from 'mongoose';
+import { CreateUserDto } from './dto/CreateUser.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  // users/:id
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new HttpException('User not found', 404);
+    const findUser = await this.userService.getUserById(id);
+    if (!findUser) throw new HttpException('User not found', 404);
+    return findUser;
+  }
+}
