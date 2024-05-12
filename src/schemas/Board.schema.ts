6c8042ecd8beb5e8fe6fb7f2f6327664644f5d18
Иван Тетеев <fenixshiro@gmail.com>
2024-05-12 17:01:20 +0300

    feat: изменил схему доски

--- src/schemas/Board.schema.ts
+++ src/schemas/Board.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { Connection } from './connection.schema';
+import { Frame } from './Frame.schema';
 import { Item } from './item.schema';
 import { User } from './user.schema';
 
@@ -12,6 +14,15 @@ export class Board {
 
   @Prop({ type: [{ type: Types.ObjectId, ref: 'Item' }], required: false })
   items: Item[];
+
+  @Prop({
+    type: [{ type: Types.ObjectId, ref: 'Connection' }],
+    required: false,
+  })
+  connections: Connection[];
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Frame' }], required: false })
+  frames: Frame[];
 
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   owner: User;
